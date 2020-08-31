2fbbc8c0ba0c892d2d6b686fe2c3f35fb7c9bcb8
Jianpeng Li <acekiseki@live.com>
2020-08-31 15:26:16 -0500

    Fixed search function to give true or false upon found or not found

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -55,6 +55,16 @@ bool LinkedList<T>::search(T value) const
 		Fix this method
 	*/
 
+	while(temp != nullptr)
+	{
+		if (temp->getValue() == value)
+		{
+			isFound = true;
+			break;
+		}
+		temp = temp -> getNext();
+	}
+	
 	return(isFound);
 }
 
